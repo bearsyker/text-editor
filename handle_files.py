f91052e097becebf92c9e4bab3496eb17cb27dba
Ian Pasquali <bearsyker@gmail.com>
2024-02-02 01:16:41 -0300

    separating files

---
+++ handle_files.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import filedialog
+
+
+def new_file(self):
+    self.add_tab()
+
+def open_file(self):
+    file_path = filedialog.askopenfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+
+    if file_path:
+        with open(file_path, "r") as file:
+            content = file.read()
+            self.add_tab()
+            current_tab = self.notebook.select()
+            text_widget = self.notebook.nametowidget(current_tab).winfo_children()[0]
+            text_widget.delete(1.0, tk.END)
+            text_widget.insert(tk.END, content)
+
+def save_file(self):
+    current_tab = self.notebook.select()
+    text_widget = self.notebook.nametowidget(current_tab).winfo_children()[0]
+
+    file_path = filedialog.asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if file_path:
+        with open(file_path, "w") as file:
+            file.write(text_widget.get(1.0, tk.END))
--- text-editor.py
+++ text-editor.py
@@ -85,36 +85,7 @@ class TextEditor:
         fonts = ["TkDefaultFont", "Helvetica", "Courier"]
         for font in fonts:
             font_menu.add_radiobutton(label=font, variable=self.font_var, value=font, command=self.update_customization)
-
-    def new_file(self):
-        self.add_tab()
-
-    def open_file(self):
-        file_path = filedialog.askopenfilename(
-            defaultextension=".txt",
-            filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
-        )
-
-        if file_path:
-            with open(file_path, "r") as file:
-                content = file.read()
-                self.add_tab()
-                current_tab = self.notebook.select()
-                text_widget = self.notebook.nametowidget(current_tab).winfo_children()[0]
-                text_widget.delete(1.0, tk.END)
-                text_widget.insert(tk.END, content)
-
-    def save_file(self):
-        current_tab = self.notebook.select()
-        text_widget = self.notebook.nametowidget(current_tab).winfo_children()[0]
-
-        file_path = filedialog.asksaveasfilename(
-            defaultextension=".txt",
-            filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
-        )
-        if file_path:
-            with open(file_path, "w") as file:
-                file.write(text_widget.get(1.0, tk.END))
+            
 
     def undo(self):
         current_tab = self.notebook.select()
